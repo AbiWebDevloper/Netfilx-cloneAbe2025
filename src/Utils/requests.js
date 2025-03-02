d4d8843406717afe2985697244a1050f4790e7c4
AbiWebDevloper <abebeone2@gmail.com>
2025-03-01 18:00:25 -0700

    request

--- src/Utils/requests.js
+++ src/Utils/requests.js
@@ -12,9 +12,10 @@
 //   getTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
 // };
 
-// // export default fetchData;
-const API_KEY = import.meta.env.VITE_API_KEY;
-// const API_KEY = process.env.REACT_APP_API_KEY;
+// // // export default fetchData;
+// const API_KEY = import.meta.env.VITE_API_KEY;
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 const fetchData = {
   getUpcommingMovies: `/movie/now_playing?language=en-US&page=1&api_key=${API_KEY}`,
   getTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
